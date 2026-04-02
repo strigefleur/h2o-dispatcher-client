5080a5c7615220afae8231bc6854d1b2e9a5fd62
Аркадий Лемачко <lemachkoani@eco.rshb.ru>
2026-04-02 16:33:08 +0300

    chore: bump version

--- Constants/EnvVariableConst.cs
+++ Constants/EnvVariableConst.cs
@@ -10,6 +10,7 @@ public static class EnvVariableConst
         new("H2O_LLM_AUTO_COMMENTER_URL", "http://localhost:11434/v1/chat/completions"),
         new("H2O_LLM_AUTO_COMMENTER_TIMEOUT", "120"),
         new("H20_LLM_AUTO_COMMENTER_PROMPT", "Верни ТОЛЬКО исходный код с добавлением кратких комментариев " +
-                                             "на русском языке; без markdown, без излишних пояснений.")
+                                             "на русском языке; без markdown, без излишних пояснений; " +
+                                             "используй summary для комментирования публичных элементов.")
     ];
 }
